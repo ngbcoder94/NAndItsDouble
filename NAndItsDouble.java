d30297e110a3fc1a4024d61c75ea03410062f47d
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-13 21:01:08 -0500

    Edge cases created

--- NAndItsDouble.java
+++ NAndItsDouble.java
@@ -20,6 +20,17 @@ public class NAndItsDouble{
          */
         public boolean checkIfExists(int[] arr){
                 
+                HashSet<Integer> set = new HashSet<>();     //Need to initialize a hashset to store items in arr[].
+
+                //Now I need to loop through the array and start the process of looking.
+                for(int i = 0; i < arr.length; i++){
+                        
+                        //Need to check the edge case of arr.length = 0 or arr.length = 1. 
+                        if(arr.length == 0 || arr.length == 1){
+                                
+                                return false;   //If arr.length is 0 or 1, then just return false.
+                        }
+                }
         }
 
         public static void main(String[] args){
